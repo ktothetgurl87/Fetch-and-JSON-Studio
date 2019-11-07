9b78406f4d9389754ea454eb07bd9a18e5985919
Katie Koski <KKoski@abstraktmg.com>
2019-11-06 20:27:07 -0600

    astronauts added

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 
 window.addEventListener("load", function(){ 
+    console.log("loaded");
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-   response.json().then(function(json) {
-      console.log(json);
-   });
+     response.json().then(function(json) {
+      const container = document.getElementById("container");
+         for(i=0; i < json.length; i++) {
+            container.innerHTML = container.innerHTML + `
+                 <div class = "astronaut">
+                     <div class = "bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active ${json[i].active}</li>
+                             <li>Skills ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                        <img class = "avatar" src = ${json[i].picture}
+                 </div>
+             `;
+             };
+     });
 });
 });
