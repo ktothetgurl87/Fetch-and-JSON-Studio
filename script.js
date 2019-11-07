07e720aae7d1dd46f56eb7d546d6163cacecebaf
Katie Koski <KKoski@abstraktmg.com>
2019-11-06 19:29:53 -0600

    console

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-window.addEventListener("load", function(){
-    fetch(https://handlers.education.launchcode.org/static/astronauts.json).then(function(response){
-        console.log();
-      });
-  })
+
+window.addEventListener("load", function(){ 
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+   response.json().then(function(json) {
+      console.log(json);
+   });
+});
+});
